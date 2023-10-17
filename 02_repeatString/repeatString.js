3a18828bc5e5a7c1c639f043aa221df286cfb83d
Yanny Patel <47578696+Yanny24211@users.noreply.github.com>
2023-10-17 08:51:09 -0400

    fixed js files

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, repNum) {
+    let result = ''; 
+    for(let i=0; i < repNum; i++){
+        result += word;     
+    }
+    return result; 
 };
 
 // Do not edit below this line
